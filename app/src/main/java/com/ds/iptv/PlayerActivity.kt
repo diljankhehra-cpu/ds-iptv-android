8ca4e145765363c5bab81c693cfbf34bc085245e
Diljan707 <diljankhehra@gmail.com>
2026-01-12 21:04:40 +0530

    Fix PlayerActivity viewBinding playerView

--- app/src/main/java/com/ds/iptv/PlayerActivity.kt
+++ app/src/main/java/com/ds/iptv/PlayerActivity.kt
@@ -1,28 +1,27 @@
 package com.ds.iptv
 
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ds.iptv.databinding.ActivityPlayerBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.ui.PlayerView
 
 class PlayerActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityPlayerBinding
     private lateinit var player: ExoPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
 
-        val url = intent.getStringExtra("url") ?: return
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         player = ExoPlayer.Builder(this).build()
-        val view = findViewById<PlayerView>(R.id.playerView)
-        view.player = player
+        binding.playerView.player = player
 
-        val media = MediaItem.fromUri(Uri.parse(url))
-        player.setMediaItem(media)
+        val url = intent.getStringExtra("url") ?: return
+        player.setMediaItem(MediaItem.fromUri(url))
         player.prepare()
         player.play()
     }
